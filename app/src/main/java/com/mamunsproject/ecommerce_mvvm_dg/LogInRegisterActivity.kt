8e7fc181dbd9c76a5ddfadcf6849404a462aedac
abmamun01 <aamamun25000@gmail.com>
2022-12-25 23:53:54 +0600

    Adding Dependencies

---
+++ app/src/main/java/com/mamunsproject/ecommerce_mvvm_dg/LogInRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.mamunsproject.ecommerce_mvvm_dg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
