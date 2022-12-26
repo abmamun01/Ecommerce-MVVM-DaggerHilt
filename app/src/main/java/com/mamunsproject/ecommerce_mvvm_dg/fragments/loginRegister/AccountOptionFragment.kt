552926952053fb9727a50c5aee98a59805777647
abmamun01 <aamamun25000@gmail.com>
2022-12-26 12:01:35 +0600

    Introduction Screen & Resources

---
+++ app/src/main/java/com/mamunsproject/ecommerce_mvvm_dg/fragments/loginRegister/AccountOptionFragment.kt
@@ -0,0 +1,17 @@
+package com.mamunsproject.ecommerce_mvvm_dg.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AccountOptionFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AccountOptionFragment : Fragment() {
+
+}
