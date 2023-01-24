49ed2feb768baaa0c9950fd106c3c732af32e1fe
Abdullah Al Mamun <aamamun25000@gmail.com>
2023-01-24 11:37:23 +0600

    BottomNavigation Setup

--- app/src/main/java/com/mamunsproject/ecommerce_mvvm_dg/activities/LogInRegisterActivity.kt
+++ app/src/main/java/com/mamunsproject/ecommerce_mvvm_dg/activities/LogInRegisterActivity.kt
@@ -1,7 +1,8 @@
-package com.mamunsproject.ecommerce_mvvm_dg
+package com.mamunsproject.ecommerce_mvvm_dg.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mamunsproject.ecommerce_mvvm_dg.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/com/mamunsproject/ecommerce_mvvm_dg/activities/ShoppingActivity.kt
+++ app/src/main/java/com/mamunsproject/ecommerce_mvvm_dg/activities/ShoppingActivity.kt
@@ -2,11 +2,22 @@ package com.mamunsproject.ecommerce_mvvm_dg.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.mamunsproject.ecommerce_mvvm_dg.R
+import com.mamunsproject.ecommerce_mvvm_dg.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
